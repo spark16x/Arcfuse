44a79b4ca9c428849d5ee0ff3b97890f337af25a
Spark16x <spark2009971@gmail.com>
2025-10-29 12:44:36 +0530

    update login-form.jsx

--- components/login-form.jsx
+++ components/login-form.jsx
@@ -1,14 +1,14 @@
-import { cn } from "@/lib/utils"
-import { Button } from "@/components/ui/button"
-import { Card, CardContent } from "@/components/ui/card"
+import { cn } from "../..//lib/utils"
+import { Button } from "../..//components/ui/button"
+import { Card, CardContent } from "../..//components/ui/card"
 import {
   Field,
   FieldDescription,
   FieldGroup,
   FieldLabel,
   FieldSeparator,
-} from "@/components/ui/field"
-import { Input } from "@/components/ui/input"
+} from "../..//components/ui/field"
+import { Input } from "../..//components/ui/input"
 
 export function LoginForm({
   className,
