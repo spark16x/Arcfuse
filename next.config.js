5924842f4cc3a652d6b3e7ccf1c246d75313c415
Spark16x <spark2009971@gmail.com>
2025-10-29 08:24:52 +0530

    update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,23 +1,5 @@
-const withPWA = require('next-pwa')({
-  dest: 'public',
-  register: true,
-  skipWaiting: true,
-  cacheId: 'arcfuse-cache-v1', // ← your PWA version here
-  runtimeCaching: [
-    {
-      urlPattern: /^https?.*/,
-      handler: 'NetworkFirst',
-      options: {
-        cacheName: 'arcfuse-runtime-cache',
-        expiration: {
-          maxEntries: 200,
-          maxAgeSeconds: 60 * 60 * 24, // 1 day
-        },
-      },
-    },
-  ],
-});
+const nextConfig = {
+  // config options here
+};
 
-module.exports = withPWA({
-  reactStrictMode: true,
-});
+export default nextConfig;
