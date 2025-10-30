2b1aa1e9e92355218e8e645c20e02bacaf4c6d9b
Spark16x <spark2009971@gmail.com>
2025-10-30 13:42:48 +0530

    update

--- app/signup/page.jsx
+++ app/signup/page.jsx
@@ -1,9 +1,17 @@
+import { GalleryVerticalEnd } from "lucide-react"
+
 import { SignupForm } from "../../components/signup-form.jsx"
 
-export default function LoginPage() {
+export default function SignupPage() {
   return (
-    <div className="bg-[var(--muted)] flex min-h-svh flex-col items-center justify-center p-6 md:p-10">
-      <div className="w-full max-w-sm md:max-w-4xl">
+    <div className="bg-muted flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10">
+      <div className="flex w-full max-w-sm flex-col gap-6">
+        <a href="#" className="flex items-center gap-2 self-center font-medium">
+          <div className="bg-primary text-primary-foreground flex size-6 items-center justify-center rounded-md">
+            <GalleryVerticalEnd className="size-4" />
+          </div>
+         Arcfuse
+        </a>
         <SignupForm />
       </div>
     </div>
